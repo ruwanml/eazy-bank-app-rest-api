9203f502aee74fde1cf76d118ac91a2410da22e2
ruwanm <ruwan.madanayake@vitalhub.com>
2024-07-09 16:49:29 +0530

    Eazy Bank REST API - Initial commit

---
+++ src/main/java/com/conceptandcoding/eazybankrestapi/EazyBankRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.conceptandcoding.eazybankrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyBankRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.conceptandcoding.eazybankrestapi.EazyBankRestApiApplication.class, args);
+	}
+
+}
